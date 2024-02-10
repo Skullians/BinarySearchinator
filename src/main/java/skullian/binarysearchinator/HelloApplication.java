979642e46a76c5b07d354ba22a9fa5068f2751c8
Skullian <133133667+Skullians@users.noreply.github.com>
2024-02-10 22:37:12 +0000

    Progress. Little by Little.
    This has taken me so long I am actually losing my mind

--- src/main/java/skullian/binarysearchinator/HelloApplication.java
+++ src/main/java/skullian/binarysearchinator/HelloApplication.java
@@ -2,18 +2,22 @@ package skullian.binarysearchinator;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("menu.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("menu.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(new Scene(root));
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("Binary Searchinator");
         stage.show();
     }
 
--- src/main/java/skullian/binarysearchinator/HelloController.java
+++ src/main/java/skullian/binarysearchinator/HelloController.java
@@ -1,14 +1,6 @@
 package skullian.binarysearchinator;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
